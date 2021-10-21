1d056545d3c97d8c4ec70d61ef14acb468bd8349
Leo-apu <34524268@fi.unju.edu.ar>
2021-10-20 23:05:33 -0300

    pequeños cambios

--- app-rick-and-morty/src/components/landing/landing.jsx
+++ app-rick-and-morty/src/components/landing/landing.jsx
@@ -6,21 +6,24 @@ import "./landing.css";
 export default class Landing extends React.Component{
     render(){
         return(
-           <Container fluid>
-               <Row className="d-flex text-white">
-                   <h2>Esta pagina web fue diseñana con el objetivo<br/>
+            <section className="landfo">
+           <div className="contland">
+               <h1>Rick and Morty</h1>
+               <div className="d-flex text-black text-center ">
+                   <h2>Esta pagina web fue diseñada con el objetivo<br/>
                        de mostrar atraves una aplicacion creada en <br/>
-                       React que no permite mostar los personaje de<br/>
-                       la Seria Rick and Morty , Buscando por nombre<br/>
-                       y tambien filtrando a eleccion los tipos de <br/>
-                       Personajes que existen en la serie.</h2>
-               </Row>
-               <Row className="d-flex align-content-around">
-                    <Button>ABOUT</Button>
-                    <Button>HOME</Button>
-                    <Button>CONTACT</Button>
-               </Row>
-           </Container>
+                       React,  los personaje de la Sere Rick and Morty<br/>
+                       Buscando por nombre y tambien filtrando a eleccion<br/>
+                        los tipos de Personajes que existen en la serie.</h2>
+                       
+               </div>
+               <div className="botn">
+                    <Button variant="Primary" size ="lg">ABOUT</Button>
+                    <Button variant="Primary" size ="lg">HOME</Button>
+                    <Button variant="Primary" size ="lg">CONTACT</Button>
+               </div>
+           </div>
+           </section>
         );
     }
 } 
