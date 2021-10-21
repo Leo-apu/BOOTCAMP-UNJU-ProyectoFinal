445adc8afd9c31cca8710935bb86721fdcdc7db9
mike47k <cristianmichel33@gmail.com>
2021-10-20 23:41:11 -0300

    Req7: subiendo rutas

--- app-rick-and-morty/src/App.js
+++ app-rick-and-morty/src/App.js
@@ -4,6 +4,11 @@ import {BrowserRouter,Switch, Route} from 'react-router-dom';
 import N from './components/navbar/navbar';
 import { useState } from 'react';
 import Character from './components/card/card.jsx';
+import About from './components/about/about';
+import Contact from './components/contact/contact';
+import LandingPage from './components/landing/landing';
+import Error from './components/error/error';
+
 function App() {
 
   const[name,setName] = useState("");
@@ -25,6 +30,20 @@ function App() {
         </Route>
         <Route exact path="/home/character/:id">
           <Character />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+
+        <Route exact path="/contacto">
+          <Contact />
+        </Route>
+
+        <Route exact path="/landingPage">
+          <LandingPage />
+        </Route>
+        <Route path="/">
+          <Error />
         </Route>
 
       </Switch>
